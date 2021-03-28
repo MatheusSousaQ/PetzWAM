b55a9aeccf40f2aaeb9e8310a6a80d49b76c82de
MatheusSousaQ <matheus.18quintanilha@gmail.com>
2021-03-27 23:21:57 -0300

    Criação da estrutura inicial(Base, Hooks, Pages(Home p/ pageFactory))

---
+++ src/test/java/pages/PageHome.java
@@ -0,0 +1,37 @@
+package pages;
+
+import general.Base;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class PageHome extends Base {
+
+    Base base;
+
+    public PageHome(WebDriver driver) {
+        super(driver);
+    }
+
+    @FindBy(id = "search")
+    private WebElement fieldInputSearch;
+
+    @FindBy(className = "button-search")
+    private WebElement buttonSearch;
+
+    public void search(String product){
+        fieldInputSearch.clear();
+        fieldInputSearch.sendKeys(product);
+    }
+
+    public void searchWithMagnifierButton(String product){
+        search(product);
+        buttonSearch.click();
+    }
+    public void searchWithMEnter(String product){
+        search(product);
+        buttonSearch.sendKeys(Keys.ENTER);
+    }
+}
